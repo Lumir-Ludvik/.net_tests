900bca7002cada1f09e54bb2764e4d54b2683676
Lumir-Ludvik <lumirludvik@email.cz>
2022-04-11 22:40:10 +0200

    strings and split

---
+++ Benchmarks/Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using Benchmarks;
+
+//Strings strings = new();
+//Console.WriteLine(strings.Span("AA472Aabce035aafs-sfsd724afa:fsss0172a$$asfd542 fsdf asfe _ -.___.---.!aeěš&@{0};"));
+//BenchmarkRunner.Run<Strings>();
+BenchmarkRunner.Run<Splits>();
+
+Console.ReadLine();
