9b7707ddcaaa0a083968e86ce4dd25ce76b34fce
Lumir-Ludvik <lumirludvik@email.cz>
2022-04-12 21:11:12 +0200

    cycle

---
+++ Benchmarks/Benchmarks/Cycle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+
+namespace Benchmarks
+{
+    [NativeMemoryProfiler]
+    [MinColumn, MaxColumn]
+    [MemoryDiagnoser]
+    public class Cycle
+    {
+
+        private int[] numInts = new int[1_000_000_000];
+
+        [Benchmark]
+        public void ForMethod()
+        {
+            for (int i = 0; i < numInts.Length; i++)
+            {
+                numInts[i] = i;
+            }
+        }
+
+        [Benchmark]
+        public void WhileMethod()
+        {
+            int i = 0;
+            while (i < numInts.Length)
+            {
+                numInts[i] = i;
+                i++;
+            }
+        }
+    }
+}
---
+++ Benchmarks/Benchmarks/Increments.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+
+namespace Benchmarks
+{
+    [NativeMemoryProfiler]
+    [MinColumn, MaxColumn]
+    [MemoryDiagnoser]
+    public class Increments
+    {
+        private int x = 0;
+
+        [Benchmark]
+        public int BasicIncrement()
+        {
+            for (var i = 0; i < 1_000_000_000; i++)
+                x = x + 1;
+
+            return x;
+        }
+
+        [Benchmark]
+        public int Precrement()
+        {
+            for (var i = 0; i < 1_000_000_000; i++)
+                ++x;
+
+            return x;
+        }
+
+        [Benchmark]
+        public int Postcrement()
+        {
+            for (var i = 0; i < 1_000_000_000; i++)
+                x++;
+
+            return x;
+        }
+
+        [Benchmark]
+        public int IncrementViaPlusEquels()
+        {
+            for (var i = 0; i < 1_000_000_000; i++)
+                x+=1;
+
+            return x;
+        }
+    }
+}
